d2c1489537c2ccf8d69face3bf247d30c1aab8a3
Shailendra Khatri <shailendrakhtr@gmail.com>
2026-01-11 23:20:20 +0530

    subString file added

---
+++ java/src/subString.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+
+public class subString {
+    public static String substring(String str , int si , int ei){
+        String substr = "";
+        for(int  i= 0;i<=ei; i++){
+            substr +=str.charAt(i);
+        }
+        return substr;
+
+    }
+    public static void main(String[] args) {
+        String str= "hello world";
+        System.out.println(substring(str, 0,5));
+        System.out.println(str.substring(0,5));
+    }
+}
