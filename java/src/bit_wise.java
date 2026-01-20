f546f182d3d3a4eb12b7097ba2a543c242726123
Shailendra Khatri <shailendrakhtr@gmail.com>
2026-01-20 22:53:02 +0530

    adding bitwise operator

---
+++ java/src/bit_wise.java
@@ -0,0 +1,21 @@
+public class bit_wise {
+    public static void main(String args[]){
+        //binary AND
+        System.out.println(5 & 6);
+
+        //binary OR
+        System.out.println(5|6);
+
+        //binary XOR
+        System.out.println(5^6);
+
+        //binary 1's complement
+        System.out.println(~5);
+
+        //binary left shift
+        System.out.println(5<<2);
+
+        //binary right shift
+        System.out.println(6>>1);
+    }
+}
