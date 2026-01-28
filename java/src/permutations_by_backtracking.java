dace298f4be1aa2ead994da47ad677fa2919bca0
Shailendra Khatri <shailendrakhtr@gmail.com>
2026-01-28 22:04:07 +0530

    commit a file for pattern

--- java/src/permutations_by_backtracking.java
+++ java/src/permutations_by_backtracking.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class permutations_by_backtracking {
+    public static void permutations(String str , String ans){
+        //base case
+        if(str.length() == 0){
+            System.out.println(ans);
+        }
+        //recursion O(n*n)!
+        for(int i=0; i< str.length( ); i++){
+            char ch = str.charAt(i);
+           String  Newstr = str.substring(0,i) + str.substring(i+1);
+            permutations(Newstr , ans+ch);
+        }
+
+    }
+    public static void main (String args[]){
+        String str = "professor";
+
+        permutations(str , "");
+    }
 }
