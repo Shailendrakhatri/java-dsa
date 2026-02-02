5ee01938349a11ca1fd8e527af8a5a801663a47b
Shailendra Khatri <shailendrakhtr@gmail.com>
2026-02-02 11:59:23 +0530

    commit a file for first non repeating character by queue

---
+++ java/src/First_Non_Repeating_Char.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class First_Non_Repeating_Char {
+    public static void PrintNonRepeating(String str){
+        int freq[] = new int[26]; //'a'-'b'
+        java.util.Queue<Character> q = new java.util.LinkedList<>();
+
+        for(int i=0; i<str.length(); i++) {
+            char ch = str.charAt(i);
+            q.add(ch);
+            freq[ch-'a']++;
+            while(!q.isEmpty() && freq[q.peek()-'a']  > 1){
+                q.remove();
+            }
+            if(q.isEmpty()) {
+                System.out.print(-1+" ");
+            }else {
+                System.out.print(q.peek()+ " ");
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        String str = "aabccxb";
+        PrintNonRepeating(str);
+    }
+}
