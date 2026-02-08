628009ddd24afff9a998ec98ab8df46303138e7b
professor <shailendrakhtr@gmail.com>
2026-02-08 20:36:09 +0530

    commit a file for maximum no of coin needed

---
+++ java/src/minimun_Indian_coin_required_greedy.java
@@ -0,0 +1,27 @@
+package src;
+import java.util.*;
+
+public class minimun_Indian_coin_required_greedy {
+    public static void main(String[] args){
+        Integer coins[] = {1,2,5,10,20,50,100,500,2000};
+
+        Arrays.sort(coins, Comparator.reverseOrder());
+        int count = 0;
+        int amount = 590;
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for(int i=0 ;i<coins.length; i++){
+            if(coins[i] <= amount) {
+                while(coins[i] <= amount) {
+                    count++;
+                    ans.add(coins[i]);
+                    amount -= coins[i];
+                }
+            }
+        }
+        System.out.println("the total no of coins are requiresd is = "+ count);
+            for(int i =0 ; i< ans.size(); i++) {
+                System.out.print(ans.get(i) +", ");
+            }
+    }
+}
