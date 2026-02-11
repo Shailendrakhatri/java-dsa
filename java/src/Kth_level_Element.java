96749150ee680e3ec9eca7f99eccc00ef5eb45e7
professor <shailendrakhtr@gmail.com>
2026-02-11 22:31:26 +0530

    commit a file to get a Kth level elements and for lowest common ancestor

---
+++ java/src/Kth_level_Element.java
@@ -0,0 +1,85 @@
+package src;
+import java.util.*;
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class Kth_level_Element {
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static void Klevel(Node root, int level, int K) {
+
+        if (root == null) {
+            return;
+        }
+
+        if (level == K) {
+            System.out.println(root.data);
+            return;
+        }
+
+        Klevel(root.left, level + 1, K);
+        Klevel(root.right, level + 1, K);
+    }
+
+    public static boolean  getPath(Node root, int n , ArrayList<Node> path){
+        if(root == null){
+            return  false;
+        }
+        path.add(root);
+        if(root.data == n ) {
+            return true;
+        }
+
+        boolean foundLeft = getPath(root.left, n , path);
+        boolean foundRight = getPath(root.right, n , path);
+        if(foundLeft || foundRight) {
+            return true;
+        }
+        path.remove(path.size()-1);
+        return false;
+
+    }
+    public static Node lca(Node root, int n1, int n2){
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+
+        int i = 0;
+        for(; i<path1.size() && i< path2.size(); i++){
+            if(path1.get(i) != path2.get(i)){
+                break;
+            }
+        }
+
+//        lca
+        Node lca = path1.get(i-1);
+        return lca;
+    }
+    public static void main(String args[]){
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+//        int k  =2;
+//         Klevel(root, 1, 3);
+        int n1 = 4, n2 = 5;
+        System.out.println(lca(root, n1, n2).data);
+
+    }
+}
