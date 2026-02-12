eb06545f4610f4102ede54eddf9ef5e4acbd629f
professor <shailendrakhtr@gmail.com>
2026-02-12 13:31:25 +0530

    commit a file for check validate binary search tree

---
+++ java/src/Validate_BST.java
@@ -0,0 +1,64 @@
+package src;
+
+public class Validate_BST {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node (int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+
+        }
+    }
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left );
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+    public static Node insert(Node root, int value){
+        if(root == null){
+            root = new Node(value);
+            return root;
+        }
+        if(root.data > value) {
+            //left subtree
+            root.left = insert(root.left, value);
+        }else{
+            //right subtree
+            root.right = insert(root.right ,value);
+        }
+        return root;
+    }
+    public static boolean isValidBST(Node root, Node min, Node max){
+        if(root == null){
+            return  true;
+        }
+        if(min != null && root.data <= min.data){
+            return false;
+        }
+        else if(max != null && root.data >= max.data){
+            return false;
+        }
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    }
+
+    public static void main(String args[]){
+        int values[] = {5,1,3,4,2,7};
+        Node root = null;
+        for(int i=0; i<values.length; i++){
+            root = insert(root, values[i]);
+        }
+        inorder(root);
+        System.out.println();
+        System.out.println(isValidBST(root, null, null));
+
+
+    }
+
+}
