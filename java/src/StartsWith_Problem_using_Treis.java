043c55ce32f7a3e274a5ab275c667fe018f45cb7
professor <shailendrakhtr@gmail.com>
2026-02-16 15:53:15 +0530

    commit a file for StartWuth problem using tries

---
+++ java/src/StartsWith_Problem_using_Treis.java
@@ -0,0 +1,52 @@
+package src;
+
+public class StartsWith_Problem_using_Treis  {
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false;
+        int freq;
+
+        public Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+            freq = 1;
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            } else {
+                curr.children[idx].freq++;
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static boolean startsWith(String prefix) {
+        Node curr = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            int idx = prefix.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        String arr[] = { "apple", "zebra", "dog", "duck", "dove" };
+        for (String word : arr) {
+            insert(word);
+        }
+        System.out.println(startsWith("do"));
+    }
+}
