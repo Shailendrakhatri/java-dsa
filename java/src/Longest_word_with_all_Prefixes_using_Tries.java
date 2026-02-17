f7cc1da36d50e20a422e9245e18369450364b90f
professor <shailendrakhtr@gmail.com>
2026-02-17 23:37:12 +0530

    commit a file for Longest_word_with_all_Prefixes_using_Tries

---
+++ java/src/Longest_word_with_all_Prefixes_using_Tries.java
@@ -0,0 +1,55 @@
+package src;
+
+public class Longest_word_with_all_Prefixes_using_Tries {
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        public Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static String ans = "";
+
+    public static void longestWord(Node root, StringBuilder temp) {
+        if (root == null) {
+            return;
+        }
+        for (int i = 0; i < root.children.length; i++) {
+            if (root.children[i] != null && root.children[i].eow == true) {
+                temp.append((char) (i + 'a'));
+                if (temp.length() > ans.length()) {
+                    ans = temp.toString();
+                }
+                longestWord(root.children[i], temp);
+                temp.deleteCharAt(temp.length() - 1);
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        String arr[] = {"a", "banana", "app", "appl", "ap", "apply", "apple"};
+        for (String word : arr) {
+            insert(word);
+        }
+        longestWord(root, new StringBuilder(""));
+        System.out.println(ans);
+    }
+}
