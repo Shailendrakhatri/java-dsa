e9e9b7118a6ebc9fe25eccb78f6412ce22ab22de
professor <shailendrakhtr@gmail.com>
2026-02-24 22:04:20 +0530

    commit a file for Leaders in an Array

---
+++ java/src/LeadersInArray.java
@@ -0,0 +1,20 @@
+package src;
+
+public class LeadersInArray {
+    public static void printLeaders(int[] arr) {
+        int maxFromRight = arr[arr.length - 1];
+        System.out.print(maxFromRight + " ");
+
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] > maxFromRight) {
+                maxFromRight = arr[i];
+                System.out.print(maxFromRight + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {16, 17, 4, 3, 5, 2};
+        printLeaders(arr);
+    }
+}
