437eddb3514384cacd09168ecafce51ac1b865f7
professor <shailendrakhtr@gmail.com>
2026-03-05 23:27:39 +0530

    commit a file for Two Sum Problem using HashMap

---
+++ java/src/TwoSum.java
@@ -0,0 +1,23 @@
+package src;
+import java.util.*;
+public class TwoSum {
+    public static void main(String[] args) {
+
+        int nums[] = {2,7,11,15};
+        int target = 9;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            if(map.containsKey(complement)) {
+                System.out.println(nums[i] + " " + complement);
+                return;
+            }
+
+            map.put(nums[i], i);
+        }
+    }
+}
