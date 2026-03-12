6a18831c7d0dc12758c48cfd5d1a0e8bb6547ca5
professor <shailendrakhtr@gmail.com>
2026-03-12 22:36:41 +0530

    commit a file to Check if Two Arrays Have Common Elements

---
+++ java/src/CommonElements.java
@@ -0,0 +1,28 @@
+package src;
+
+
+import java.util.HashSet;
+
+public class CommonElements {
+
+    public static void main(String[] args) {
+
+        int arr1[] = {1,2,3,4};
+        int arr2[] = {5,6,3,8};
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : arr1){
+            set.add(num);
+        }
+
+        for(int num : arr2){
+            if(set.contains(num)){
+                System.out.println("Common element found: " + num);
+                return;
+            }
+        }
+
+        System.out.println("No common element");
+    }
+}
