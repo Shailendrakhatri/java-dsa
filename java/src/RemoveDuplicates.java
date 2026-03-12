dd7f47dfe203a588e9bd02c4a6c4038867d4e37e
professor <shailendrakhtr@gmail.com>
2026-03-12 22:34:28 +0530

    commit a file for Remove Duplicate Elements from an Array

---
+++ java/src/RemoveDuplicates.java
@@ -0,0 +1,18 @@
+package src;
+
+import java.util.HashSet;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+
+        int arr[] = {1,2,3,2,4,1,5};
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : arr){
+            set.add(num);
+        }
+
+        System.out.println("Unique elements: " + set);
+    }
+}
