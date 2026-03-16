72ddeadc3bd40bf959b3d2ca768bb29f75b8e9ea
professor <shailendrakhtr@gmail.com>
2026-03-16 23:52:19 +0530

    commit a file to Count Valid Triangles

---
+++ java/src/ValidTriangles.java
@@ -0,0 +1,35 @@
+package src;
+import java.util.Arrays;
+
+public class ValidTriangles {
+    public static int countTriangles(int[] arr) {
+
+        Arrays.sort(arr);
+        int count = 0;
+
+        for(int i = arr.length - 1; i >= 2; i--) {
+
+            int left = 0;
+            int right = i - 1;
+
+            while(left < right) {
+
+                if(arr[left] + arr[right] > arr[i]) {
+                    count += right - left;
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {4,6,3,7};
+
+        System.out.println(countTriangles(arr));
+    }
+}
