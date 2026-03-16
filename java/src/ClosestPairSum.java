cf594dd1a5d47c2313885acf4ab7037971982a9c
professor <shailendrakhtr@gmail.com>
2026-03-16 23:50:30 +0530

    commit a file toFind Closest Pair Sum to Target

---
+++ java/src/ClosestPairSum.java
@@ -0,0 +1,37 @@
+package src;
+import java.util.Arrays;
+
+public class ClosestPairSum {
+    public static int closestSum(int[] arr, int target) {
+
+        Arrays.sort(arr);
+
+        int left = 0;
+        int right = arr.length - 1;
+        int closest = Integer.MAX_VALUE;
+
+        while(left < right) {
+
+            int sum = arr[left] + arr[right];
+
+            if(Math.abs(target - sum) < Math.abs(target - closest)) {
+                closest = sum;
+            }
+
+            if(sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return closest;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,4,7,10};
+
+        System.out.println(closestSum(arr, 15));
+    }
+}
