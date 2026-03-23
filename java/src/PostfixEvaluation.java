5c6c2262bdd675e1f750fcecf717bbe318bcd7d3
professor <shailendrakhtr@gmail.com>
2026-03-23 23:20:56 +0530

    commit a file  to Evaluate Postfix Expression

---
+++ java/src/PostfixEvaluation.java
@@ -0,0 +1,25 @@
+package src;
+import java.util.Stack;
+
+public class PostfixEvaluation {
+    public static int evaluate(String exp) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (char ch : exp.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                stack.push(ch - '0');
+            } else {
+                int b = stack.pop();
+                int a = stack.pop();
+
+                switch (ch) {
+                    case '+': stack.push(a + b); break;
+                    case '-': stack.push(a - b); break;
+                    case '*': stack.push(a * b); break;
+                    case '/': stack.push(a / b); break;
+                }
+            }
+        }
+        return stack.pop();
+    }
+}
