3cd3fff6adadf6acdbd18b0b7b08fa83fd428227
professor <shailendrakhtr@gmail.com>
2026-03-26 23:25:14 +0530

    commit a file  for task Scheduler  problem

---
+++ java/src/TaskScheduler.java
@@ -0,0 +1,20 @@
+package src;
+import java.util.*;
+public class TaskScheduler {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq = new int[26];
+
+        for (char t : tasks) freq[t - 'A']++;
+
+        Arrays.sort(freq);
+
+        int max = freq[25] - 1;
+        int idle = max * n;
+
+        for (int i = 24; i >= 0 && freq[i] > 0; i--) {
+            idle -= Math.min(freq[i], max);
+        }
+
+        return idle > 0 ? idle + tasks.length : tasks.length;
+    }
+}
