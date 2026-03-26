8232d34b3b8a3d4d3230d50cc739b71db33f89fa
professor <shailendrakhtr@gmail.com>
2026-03-26 23:29:07 +0530

    commit a file  for Minimum Cost to Connect Ropes problem

---
+++ java/src/ConnectRopes.java
@@ -0,0 +1,25 @@
+package src;
+import java.util.*;
+
+
+public class ConnectRopes {
+    public int minCost(int[] ropes) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int r : ropes) pq.offer(r);
+
+        int cost = 0;
+
+        while (pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
+
+            int sum = a + b;
+            cost += sum;
+
+            pq.offer(sum);
+        }
+
+        return cost;
+    }
+}
