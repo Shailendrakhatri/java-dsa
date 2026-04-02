8936e0061339877d09633f49d37d2fab09814222
professor <shailendrakhtr@gmail.com>
2026-04-02 23:45:10 +0530

    commit a file  for practice dsa

--- java/src/Assign_Cookies.java
+++ java/src/Assign_Cookies.java
@@ -6,20 +6,22 @@ public class Assign_Cookies {
             Arrays.sort(g);
             Arrays.sort(s);
 
+
             int i = 0, j = 0;
 
             while (i < g.length && j < s.length) {
 
                 if (s[j] >= g[i]) {
-                    i++;
+                           i++;
                 }
                 j++;
-            }
+
+             }
 
             return i;
         }
 
-        public static void main(String[] args) {
+          public static void main(String[] args) {
 
             int[] greed = {1, 2, 3};
             int[] cookies = {1, 1};
