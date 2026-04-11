b9db1d5347b61e15ec2eae607fda302a70eaba65
professor <shailendrakhtr@gmail.com>
2026-04-11 23:52:12 +0530

    commit a file  for practice dsa

--- java/src/ValidParentheses.java
+++ java/src/ValidParentheses.java
@@ -5,7 +5,6 @@ public class ValidParentheses {
 
     public static boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-
         for (char ch : s.toCharArray()) {
             if (ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
