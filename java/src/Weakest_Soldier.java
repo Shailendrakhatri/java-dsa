86dc305fef8ef113d3399f666ea6d7ff06541f82
professor <shailendrakhtr@gmail.com>
2026-04-11 23:46:00 +0530

    commit a file  for practice dsa

--- java/src/Weakest_Soldier.java
+++ java/src/Weakest_Soldier.java
@@ -10,18 +10,17 @@ public class Weakest_Soldier {
             this.soldiers = soldiers;
             this.idx = idx;
         }
+
+
         @Override
         public int compareTo(Row r2){
-        if(this.soldiers == r2.soldiers) {
-            return this.idx - r2.idx;
-        }else{
-            return this.soldiers - r2.soldiers;
+            if(this.soldiers == r2.soldiers) {
+                return this.idx - r2.idx;
+            }else{
+                return this.soldiers - r2.soldiers;
+            }
         }
         }
-
-
-        }
-
 
     public static void main(String args[]) {
         int army[][] = {{1, 0, 0, 0},
