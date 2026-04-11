afe7355925d7f4263dc94266d7ebecaf39965484
professor <shailendrakhtr@gmail.com>
2026-04-11 23:48:44 +0530

    commit a file  for practice dsa

--- java/src/ValidTriangles.java
+++ java/src/ValidTriangles.java
@@ -7,13 +7,9 @@ public class ValidTriangles {
 
         int count = 0;
         for(int i = arr.length - 1; i >= 2; i--) {
-
             int left = 0;
             int right = i - 1;
-
             while(left < right) {
-
-
                 if(arr[left] + arr[right] > arr[i]) {
                     count += right - left;
                     right--;
