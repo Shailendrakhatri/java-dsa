96c814f1078c3590b0351f096d0d4e76fe95b42f
professor <shailendrakhtr@gmail.com>
2026-04-12 23:13:36 +0530

    commit a file  for practice dsa

--- java/src/bubble_sort.java
+++ java/src/bubble_sort.java
@@ -12,16 +12,12 @@ public class bubble_sort {
 
         }
     }
-
     public static void printarray(int arr[]){
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i] +" ");
         }
         System.out.println();
     }
-
-
-
     public static void main(String[] args) {
         int arr[] = {5,4,1,3,2,};
         bubblesort(arr);
