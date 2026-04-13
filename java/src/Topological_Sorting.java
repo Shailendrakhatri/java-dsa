1d541e4be1ea69e14ee972973382975498a821d6
professor <shailendrakhtr@gmail.com>
2026-04-13 23:13:18 +0530

    commit a file  for practice dsa

--- java/src/Topological_Sorting.java
+++ java/src/Topological_Sorting.java
@@ -15,12 +15,9 @@ public class Topological_Sorting {
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
-
         graph[0].add(new Edge(0, 2));
         graph[0].add(new Edge(0, 3));
-
         graph[1].add(new Edge(1, 0));
-
         graph[2].add(new Edge(2, 3));
         graph[2].add(new Edge(3, 0));
     }
