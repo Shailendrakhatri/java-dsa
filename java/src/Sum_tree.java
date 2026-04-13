37f0261005a836d4212c407a6be5fdc6fc6bad4d
professor <shailendrakhtr@gmail.com>
2026-04-13 23:17:59 +0530

    commit a file  for practice dsa

--- java/src/Sum_tree.java
+++ java/src/Sum_tree.java
@@ -11,17 +11,16 @@ public class Sum_tree {
             this.right = null;
         }
     }
+
     public static int  transform(Node root){
         if(root == null){
             return 0;
         }
         int leftChild = transform(root.left);
         int rightChild = transform(root.right);
-
         int data = root.data;
         int newLeft = root.left == null ? 0 : root.left.data;
         int newRight = root.right == null ? 0 : root.right.data;
-
         root.data = newLeft+ leftChild + newRight + rightChild;
         return data;
     }
