6bb81d0956268c7bb02d07b1b53274900395253a
professor <shailendrakhtr@gmail.com>
2026-04-17 23:53:35 +0530

    commit a file  for practice dsa

--- java/src/Chocola_problem_by_greedy.java
+++ java/src/Chocola_problem_by_greedy.java
@@ -4,18 +4,16 @@ public class Chocola_problem_by_greedy {
     public static void main(String args[]){
         int n= 4;
         int m = 6;
+
         Integer costVar[] = {2,1,3,1,4};
         Integer costHor[] = {4,1,2};
-
         Arrays.sort(costVar, Collections.reverseOrder());
         Arrays.sort(costHor, Collections.reverseOrder());
-
         int h =0 ;
         int v =0 ;
         int hp =1;
         int vp = 1;
         int cost = 0;
-
         while(h< costHor.length && v < costVar.length){
             //vertical cost is < horizontal cost
             if(costVar[v] <= costHor[h]){
