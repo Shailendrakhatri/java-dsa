5d7e7897682e5cbcc5f14310a388b17654889bbc
professor <shailendrakhtr@gmail.com>
2026-04-19 23:46:46 +0530

    commit a file  for practice dsa

--- java/src/NextSmallerElement.java
+++ java/src/NextSmallerElement.java
@@ -9,6 +9,7 @@ public class NextSmallerElement {
             while (!stack.isEmpty() && stack.peek() >= arr[i]) {
                 stack.pop();
             }
+
             int nse = stack.isEmpty() ? -1 : stack.peek();
             System.out.println(arr[i] + " -> " + nse);
             stack.push(arr[i]);
