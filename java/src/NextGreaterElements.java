ac50ee74d647e0027ea3b492879bc17c7acb8e32
professor <shailendrakhtr@gmail.com>
2026-04-19 23:38:43 +0530

    commit a file  for practice dsa

--- java/src/NextGreaterElements.java
+++ java/src/NextGreaterElements.java
@@ -3,17 +3,16 @@ import java.util.*;
 
 public class NextGreaterElements {
     public static void findNGE(int[] arr) {
-        Stack<Integer> stack = new Stack<>();
 
+        Stack<Integer> stack = new Stack<>();
         for (int i = arr.length - 1; i >= 0; i--) {
             while (!stack.isEmpty() && stack.peek() <= arr[i]) {
                 stack.pop();
             }
-
             int nge = stack.isEmpty() ? -1 : stack.peek();
             System.out.println(arr[i] + " -> " + nge);
-
             stack.push(arr[i]);
+
         }
     }
 }
