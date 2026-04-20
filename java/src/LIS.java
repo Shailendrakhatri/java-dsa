00f8fd358ed3df0b3d32980e43447d2a0fb3d0c9
professor <shailendrakhtr@gmail.com>
2026-04-20 23:51:29 +0530

    commit a file  for practice dsa

--- java/src/LIS.java
+++ java/src/LIS.java
@@ -8,9 +8,7 @@ public class LIS {
         int n = nums.length;
         int[] dp = new int[n];
         Arrays.fill(dp, 1);
-
         int max = 1;
-
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
