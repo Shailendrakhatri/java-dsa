2374ce6e797ef507255b063baf840b5d44d94612
professor <shailendrakhtr@gmail.com>
2026-05-02 23:29:45 +0530

    commit a file  for practice dsa

--- java/src/DequeExample.java
+++ java/src/DequeExample.java
@@ -3,11 +3,9 @@ import java.util.*;
 public class DequeExample {
     public static void main(String[] args) {
         Deque<Integer> dq = new ArrayDeque<>();
-
         dq.addFirst(10);
         dq.addLast(20);
         dq.addLast(30);
-
         System.out.println(dq.removeFirst()); // 10
         System.out.println(dq.removeLast());  // 30
     }
