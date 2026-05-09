be2250250c123180cb035bf3f70949fb98d62c28
professor <shailendrakhtr@gmail.com>
2026-05-09 23:16:17 +0530

    commit a file  for practice dsa

--- java/src/FibonacciDP.java
+++ java/src/FibonacciDP.java
@@ -13,10 +13,8 @@ public class FibonacciDP {
         for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
-
         return dp[n];
     }
-
     public static void main(String[] args) {
         int n = 10;
         System.out.println("Fibonacci number is: " + fibonacci(n));
