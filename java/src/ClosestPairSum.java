dacaaba672a93bdffb83c45eaeebe644bd191212
professor <shailendrakhtr@gmail.com>
2026-05-16 23:45:48 +0530

    commit a file  for practice dsa

--- java/src/ClosestPairSum.java
+++ java/src/ClosestPairSum.java
@@ -25,6 +25,7 @@ public class ClosestPairSum {
         }
         return closest;
     }
+
     public static void main(String[] args) {
         int[] arr = {1,4,7,10};
         System.out.println(closestSum(arr, 15));
