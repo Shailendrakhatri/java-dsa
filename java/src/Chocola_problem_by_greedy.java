199662cc18fb973541220f179c0db921009d0cc1
professor <shailendrakhtr@gmail.com>
2026-05-21 23:47:36 +0530

    commit a file  for practice dsa

--- java/src/Chocola_problem_by_greedy.java
+++ java/src/Chocola_problem_by_greedy.java
@@ -26,11 +26,13 @@ public class Chocola_problem_by_greedy {
                 v++;
             }
         }
+
         while(h < costHor.length){
             cost += (costHor[h] *vp);
             hp++;
             h++;
         }
+
         while(v< costVar.length){
             cost += (costVar[v] * hp);
             vp++;
