3d4289bf58873423569504749fa79ab24e24ba40
professor <shailendrakhtr@gmail.com>
2026-05-26 23:35:54 +0530

    commit a file  for practice dsa

--- java/src/BFSGraph.java
+++ java/src/BFSGraph.java
@@ -11,16 +11,10 @@ public class BFSGraph {
         g.get(0).add(2);
         g.get(1).add(3);
         g.get(2).add(3);
-
-
-
         boolean v[] = new boolean[4];
         Queue<Integer> q = new LinkedList<>();
-
         q.add(0);
         v[0] = true;
-
-
         while(!q.isEmpty()){
             int n = q.poll();
             System.out.print(n+" ");
